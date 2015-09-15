5d592909b3f256e67ee783e0d013e3fb55a873bb
Rachel White <rachelwhite@rachels-mbp-2.usny.ibm.com>
2015-09-15 16:48:06 -0400

    adding packages and setting up gulp plugins

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+var gulp = require('gulp');
+var uncss = require('gulp-uncss');
+
+gulp.task('default', function() {
+  // place code for your default task here
+  // todo: add rules for gulp-uncss https://www.npmjs.com/package/gulp-uncss
+});
